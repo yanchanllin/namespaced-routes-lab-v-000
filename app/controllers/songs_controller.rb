4f0709e74018453d53fdda99bfea60613aa54581
YanC Lin <yonalin1@gmail.com>
2018-04-24 21:08:35 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -80,4 +80,8 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> a135af3ef75dd641898595c6faf4a7ce7f1f2030
